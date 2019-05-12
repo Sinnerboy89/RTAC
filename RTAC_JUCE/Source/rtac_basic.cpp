faeb4bcafacaf7e0f745036fef5710c8097e438c
Chris Buchanan <christopher.george.buchanan@gmail.com>
2019-05-12 12:08:06 +0100

    myHRTFConvolver port done for single channel

--- RTAC_JUCE/Source/rtac_basic.cpp
+++ RTAC_JUCE/Source/rtac_basic.cpp
@@ -23,11 +23,12 @@ AudioBuffer<float>* MainComponent::myHRTFConvolver(AudioBuffer<float>* s1, Audio
 	/*  %% This function takes in input as the 5.1 audio buffers and their corresponding hrtfs
 		%  Performs convolution for all the channels and downmixes it to binaural */
 
-	AudioBuffer<float> y_buf;
-	dsp::AudioBlock<float> y_block (y_buf);
+	AudioBuffer<float>* y;
+	AudioBuffer<float> cnvFR_out_buf;
+	dsp::AudioBlock<float> cnvFR_out_block (cnvFR_out_buf);
 	dsp::ProcessSpec PS;
 	dsp::Convolution cnvFR;
-	juce::dsp::ProcessContextReplacing<float> PC (y_block);
+	juce::dsp::ProcessContextReplacing<float> PC (cnvFR_out_block);
 
 	double Fs = 44100;
 	double partitionLength = 1024;
@@ -46,37 +47,8 @@ AudioBuffer<float>* MainComponent::myHRTFConvolver(AudioBuffer<float>* s1, Audio
 	// Apply convolutions
 	cnvFR.process(PC);
 
-	//	%% Applying convolution for Front Left locations
-	//	cnvFLOutL = cnvFL_L(signal1);
-	//cnvFLOutR = cnvFL_R(signal1);
+	// Sum outputs (including LFE)
+	y->copyFrom(0, 0, cnvFR_out_buf.getReadPointer(0), cnvFR_out_buf.getNumSamples());
 
-
-	//%% Applying convolution for Front Right locations
-	//	cnvFROutL = cnvFR_L(signal2);
-	//cnvFROutR = cnvFR_R(signal2);
-
-	//%% Applying convolution for Center locations
-	//	cnvCOutL = cnvC_L(signal3);
-	//cnvCOutR = cnvC_R(signal3);
-
-
-	//%% Applying convolution for Rear Left locations
-	//	cnvRLOutL = cnvRL_L(signal5);
-	//cnvRLOutR = cnvRL_R(signal5);
-
-	//%% Applying convolution for Rear Right locations
-	//	cnvRROutL = cnvRR_L(signal6);
-	//cnvRROutR = cnvRR_R(signal6);
-
-	//%% Summing all the outputs including LFE(signal4)
-	//	cnvOutL = cnvFROutL + cnvFLOutL + cnvCOutL + cnvRROutL + cnvRLOutL + signal4;
-	//cnvOutR = cnvFROutR + cnvFLOutR + cnvCOutR + cnvRROutR + cnvRLOutR + signal4;
-
-	//%% creating the binaural stereo file for playback
-	//	cnvOut = [cnvOutL cnvOutR];
-
-	//end
-
-	// dummy return
-	return s1;
+	return y;
 }
